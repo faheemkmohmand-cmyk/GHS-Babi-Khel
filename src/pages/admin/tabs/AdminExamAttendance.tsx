a6bd98c2fdb9547ad3915a4fd87bb215b8c24592
ghs-babi-khel <faheemk.mohmand@gmail.com>
2026-05-27 12:07:32 +0500

    Fix formatting issues in AdminExamAttendance.tsx

--- src/pages/admin/tabs/AdminExamAttendance.tsx
+++ src/pages/admin/tabs/AdminExamAttendance.tsx
@@ -370,7 +370,7 @@ const AdminExamAttendance = () => {
                     })}
                   </div>
 
-                  {/* Desktop table */}
+                {/* Desktop table */}
                   <div className="hidden sm:block">
                     <Card><CardContent className="p-0 overflow-x-auto">
                       <table className="w-full text-sm">

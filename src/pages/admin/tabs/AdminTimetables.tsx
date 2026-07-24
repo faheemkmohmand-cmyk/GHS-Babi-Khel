ad40acd0aa63d58957ffe8a969fdc2b3caa74be8
Faheem Khan <faheemk.mohmand@gmail.com>
2026-07-24 19:13:51 +0500

    Update AdminTimetables.tsx

--- src/pages/admin/tabs/AdminTimetables.tsx
+++ src/pages/admin/tabs/AdminTimetables.tsx
@@ -12,7 +12,7 @@ import {
 } from "@/components/ui/alert-dialog";
 import {
   Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle,
-} from "@/components/ui/dialog";
+} from "@/components/ui/dialog"; 
 import {
   Select, SelectContent, SelectItem, SelectTrigger, SelectValue,
 } from "@/components/ui/select";

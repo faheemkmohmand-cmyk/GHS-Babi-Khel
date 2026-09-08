d026624448341b1c9255fe7da55dabb937765b4c
Faheem Khan <faheemk.mohmand@gmail.com>
2026-09-07 22:27:32 -0700

    Add files via upload

---
+++ src/components/ReportCard/index.ts
@@ -0,0 +1,12 @@
+// src/components/ReportCard/index.ts
+// Barrel export for the Report Card feature.
+//
+// Usage in Home.tsx:
+//   import ReportCardButton from "@/components/ReportCard";
+//   ...
+//   <ReportCardButton />
+
+export { default } from "./ReportCardButton";
+export { default as ReportCardButton } from "./ReportCardButton";
+export { default as ReportCardModal } from "./ReportCardModal";
+export * from "./types";
